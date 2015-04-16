df5443d9b2cc5006e5d74f8cebe4089e873987f2
Spruill1 <david.spruill@duke.edu>
2015-04-16 02:42:15 -0400

    accepting the inputs to the program

--- proxy.c
+++ proxy.c
@@ -6,3 +6,15 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int port;
+int cache_size;
+
+int main(int argc, char* argv[]){
+    if(argc < 3) {
+        perror("not enough inputs!");
+        return -1;
+    }
+
+    port = atoi(argv[1]);
+    cache_size = atoi(argv[2]);
+}
