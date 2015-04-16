c858035b00038caccddc01d69c45f34eb7451868
Spruill1 <david.spruill@duke.edu>
2015-04-16 02:59:30 -0400

    began working on getting incoming connections received

--- proxy.c
+++ proxy.c
@@ -6,8 +6,37 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define MB 1000000
+
 int port;
-int cache_size;
+int cache_size; // in megabytes
+
+char *cache;
+
+int connection(){
+    int sock;
+    struct sockaddr_in addr;
+    addr.sin_family = PF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+
+    if((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+        perror("Could not create socket");
+        return -1;
+    }
+    if((bind(sock, (struct sockaddr *) &addr, sizeof(addr))) < 0) {
+        perror("Could not bind socket");
+        return -1;
+    }
+    if((listen(sock,10))<0){
+        perror("Could not listen");
+        return -1;
+    }
+
+    //handle incoming connections
+
+    return 0;
+}
 
 int main(int argc, char* argv[]){
     if(argc < 3) {
@@ -17,4 +46,13 @@ int main(int argc, char* argv[]){
 
     port = atoi(argv[1]);
     cache_size = atoi(argv[2]);
+
+    if(port < 1024 || port > 65535) {
+        perror("please use a port number between 1024 and 65535");
+        return -1;
+    }
+
+    cache = (char*)malloc(cache_size*MB);
+
+    return connection();
 }
